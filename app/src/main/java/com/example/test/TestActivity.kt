b83006322aa70b89f4a70cbd4cb13f9796e80c92
Александр Ламожапов <vikal1709@gmail.com>
2024-01-23 23:44:07 +0800

    Обработка нажатий на кнопку

--- app/src/main/java/com/example/test/TestActivity.kt
+++ app/src/main/java/com/example/test/TestActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 
 class TestActivity : AppCompatActivity() {
+    var gender : Char = 'м'
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
@@ -13,5 +15,12 @@ class TestActivity : AppCompatActivity() {
      fun back(v : View){
          val intent = Intent(this, MainActivity::class.java)
          startActivity(intent)
+
      }
+    fun checkGender(v : View){
+        gender = if(v.id == R.id.genderM) 'м' else 'ж'
+        println(gender)
+
+    }
+
 }
